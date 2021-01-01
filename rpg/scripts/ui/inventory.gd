deb647644a6fadff5408398c9b220a75d5659c07
Sergio Sanchez <ronero.canario@gmail.com>
2021-01-02 00:30:31 +0100

    improving inventory system

--- rpg/scripts/ui/inventory.gd
+++ rpg/scripts/ui/inventory.gd
@@ -17,14 +17,9 @@ func _ready():
 	for i in range(l_slots.size()):
 		l_slots[i].connect("gui_input", self, "loot_slot_gui_input", [l_slots[i]])
 		l_slots[i].loot_slot_index = i
-	
-	
+
 	initialize_inventory()
-	
-#	for e in range(l_slots.size()):
-#		l_slots[e].connect("gui_input", self, "loot_slot_gui_input", [l_slots[e]])
-#		l_slots[e].loot_slot_index = e
-#	initialize_inventory()
+
 
 func initialize_inventory():
 	var i_slots = inventory_slots.get_children()
@@ -38,24 +33,13 @@ func initialize_inventory():
 		if PlayerInventory.loot_inventory.has(i):
 			l_slots[i].initialize_item(PlayerInventory.loot_inventory[i][0], PlayerInventory.loot_inventory[i][1])
 
-func check():
-	if PlayerInventory.loot_inventory.size() == 0:
-		print("Loot:")
-		print("No items yet on Loot")
-	else:
-		print("")
-		print("Loot:")
-		for key in PlayerInventory.loot_inventory:
-			print("%s: %s" % [PlayerInventory.loot_inventory[key][0],PlayerInventory.loot_inventory[key][1]])
-	if PlayerInventory.inventory.size() == 0:
-		print("Inventory:")
-		print("No items yet on Inventory")
-	else:
-		print("")
-		print("Inventory:")
-		for key in PlayerInventory.inventory:
-			print("%s: %s" % [PlayerInventory.inventory[key][0],PlayerInventory.inventory[key][1]])
-############### INVENTORY CODE STARTS #################
+
+func _input(_event):
+	if holding_item:
+		holding_item.global_position = get_global_mouse_position()
+
+
+############### INVENTORY CODE STARTS HERE #################
 
 func slot_gui_input(event: InputEvent, slot: SlotClass):
 	if event is InputEventMouseButton:
@@ -75,17 +59,14 @@ func slot_gui_input(event: InputEvent, slot: SlotClass):
 						left_click_same_item(slot)
 			elif slot.item:
 				left_click_not_holding(slot)
-			check()
 
-func _input(_event):
-	if holding_item:
-		holding_item.global_position = get_global_mouse_position()
 
 func left_click_empty_slot(slot: SlotClass):
 	PlayerInventory.add_item_to_empty_slot(holding_item, slot)
 	slot.putIntoSlot(holding_item)
 	holding_item = null
-	
+
+
 func left_click_different_item(event: InputEvent, slot: SlotClass):
 	PlayerInventory.remove_item(slot)
 	PlayerInventory.add_item_to_empty_slot(holding_item, slot)
@@ -94,6 +75,7 @@ func left_click_different_item(event: InputEvent, slot: SlotClass):
 	temp_item.global_position = event.global_position
 	slot.putIntoSlot(holding_item)
 	holding_item = temp_item
+
 
 func left_click_same_item(slot: SlotClass):
 	var stack_size = int(Data.item_data[slot.item.item_name]["stack_size"])
@@ -107,7 +89,8 @@ func left_click_same_item(slot: SlotClass):
 		PlayerInventory.add_item_quantity(slot, able_to_add)
 		slot.item.add_item_quantity(able_to_add)
 		holding_item.decrease_item_quantity(able_to_add)
-		
+
+
 func left_click_not_holding(slot: SlotClass):
 	PlayerInventory.remove_item(slot)
 	holding_item = slot.item
@@ -116,7 +99,9 @@ func left_click_not_holding(slot: SlotClass):
 
 ############### INVENTORY CODE ENDS #################
 
-############### LOOT CODE STARTS #################
+
+
+############### LOOT CODE STARTS HERE #################
 
 func loot_slot_gui_input(event: InputEvent, slot: SlotClass):
 	if event is InputEventMouseButton:
@@ -137,12 +122,13 @@ func loot_slot_gui_input(event: InputEvent, slot: SlotClass):
 			# ... not holding an item
 			elif slot.item:
 				loot_left_click_not_holding(slot)
-			check()
+
 
 func loot_left_click_empty_slot(slot: SlotClass):
 	PlayerInventory.loot_add_item_to_empty_slot(holding_item, slot)
 	slot.putIntoSlot(holding_item)
 	holding_item = null
+
 
 func loot_left_click_different_item(event: InputEvent, slot: SlotClass):
 	PlayerInventory.loot_remove_item(slot)
@@ -152,6 +138,7 @@ func loot_left_click_different_item(event: InputEvent, slot: SlotClass):
 	temp_item.global_position = event.global_position
 	slot.putIntoSlot(holding_item)
 	holding_item = temp_item
+
 
 func loot_left_click_same_item(slot: SlotClass):
 	var stack_size = int(Data.item_data[slot.item.item_name]["stack_size"])
@@ -165,6 +152,7 @@ func loot_left_click_same_item(slot: SlotClass):
 		PlayerInventory.loot_add_item_quantity(slot, able_to_add)
 		slot.item.add_item_quantity(able_to_add)
 		holding_item.decrease_item_quantity(able_to_add)
+
 
 func loot_left_click_not_holding(slot: SlotClass):
 	PlayerInventory.loot_remove_item(slot)
